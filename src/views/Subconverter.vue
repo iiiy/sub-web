5cf96cfa9355303fff95e35eaeb7ebfe1a2b8069
iiy <24689160+iiiy@users.noreply.github.com>
2021-06-28 21:59:24 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,7 +255,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "http://127.0.0.1:25500/sub?" }],
+        backendOptions: [{ value: "https://shellclash.herokuapp.com?" }],
         remoteConfig: [
           {
             label: "universal",
